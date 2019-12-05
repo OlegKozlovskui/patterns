e7cacb68db588abce68f69f90cec94e16ab48fd4
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2019-12-05 23:43:58 +0200

    decorator pattern

---
+++ structual/decorator.js
@@ -0,0 +1,21 @@
+class Server {
+  constructor(ip, port) {
+    this.ip = ip;
+    this.port = port;
+  }
+
+  get url() {
+    return `https://${this.ip}:${this.port}`;
+  }
+}
+
+function aws(server) {
+  server.isAWS = true;
+  server.awsInfo = function () {
+    return server.url;
+  };
+
+  return server;
+}
+
+const s1 = aws(new Server('12.255.255.255', 8080));
